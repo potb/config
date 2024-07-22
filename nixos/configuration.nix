705533afb0f146486170a7b19fabe3dc9a484f3e
Peïo Thibault <peio.thibault@gmail.com>
2024-07-22 20:01:03 +0200

    secure boot - todo option for first setup

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -42,8 +42,13 @@
   };
 
   # Boot settings
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/etc/secureboot";
+  };
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.binfmt.emulatedSystems = ["aarch64-linux"];
 
   # Hardware settings
   hardware.opengl = {
