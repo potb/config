5d53850d8651e661da09f265f50c03e9a14a675b
Peïo Thibault <peio.thibault@gmail.com>
2025-07-14 01:03:39 +0200

    feat: switch to alacritty with antidote for zsh (#3)

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -11,6 +11,38 @@
     flavor = "latte";
   };
 
+  home.file.".zsh_plugins.txt".text = ''
+    mattmc3/ez-compinit
+    zsh-users/zsh-completions kind:fpath path:src
+    getantidote/use-omz
+    ohmyzsh/ohmyzsh path:lib
+    ohmyzsh/ohmyzsh path:plugins/archlinux
+    ohmyzsh/ohmyzsh path:plugins/aws kind:defer
+    ohmyzsh/ohmyzsh path:plugins/bun kind:defer
+    ohmyzsh/ohmyzsh path:plugins/colored-man-pages kind:defer
+    ohmyzsh/ohmyzsh path:plugins/common-aliases
+    ohmyzsh/ohmyzsh path:plugins/docker
+    ohmyzsh/ohmyzsh path:plugins/docker-compose
+    ohmyzsh/ohmyzsh path:plugins/extract
+    ohmyzsh/ohmyzsh path:plugins/eza
+    ohmyzsh/ohmyzsh path:plugins/fancy-ctrl-z
+    ohmyzsh/ohmyzsh path:plugins/git
+    ohmyzsh/ohmyzsh path:plugins/git-commit kind:defer
+    ohmyzsh/ohmyzsh path:plugins/gpg-agent
+    ohmyzsh/ohmyzsh path:plugins/heroku kind:defer
+    ohmyzsh/ohmyzsh path:plugins/magic-enter
+    ohmyzsh/ohmyzsh path:plugins/node kind:defer
+    ohmyzsh/ohmyzsh path:plugins/ssh kind:defer
+    ohmyzsh/ohmyzsh path:plugins/ssh-agent
+    ohmyzsh/ohmyzsh path:plugins/sudo
+    ohmyzsh/ohmyzsh path:plugins/starship
+    ohmyzsh/ohmyzsh path:plugins/systemd
+    ohmyzsh/ohmyzsh path:plugins/transfer kind:defer
+    ohmyzsh/ohmyzsh path:plugins/zoxide
+    zsh-users/zsh-autosuggestions
+    zdharma-continuum/fast-syntax-highlighting kind:defer
+  '';
+
   home = let
     homePath = "/home/potb";
   in {
@@ -44,8 +76,22 @@
 
       initExtraFirst = "source /etc/zsh/zshrc";
 
-      initExtra = ''
+      initExtraFirst = ''
+               source /etc/zsh/zshrc
+               source '/usr/share/zsh-antidote/antidote.zsh'
+
+               zstyle ':omz:plugins:eza' 'dirs-first' yes
+               zstyle ':omz:plugins:eza' 'git-status' yes
+               zstyle ':omz:plugins:eza' 'header' yes
+               zstyle ':omz:plugins:eza' 'icons' yes
+
+        export MAGIC_ENTER_OTHER_COMMAND='ls -lah .'
+
+        antidote load
+
         eval "$(${pkgs.fnm}/bin/fnm env --use-on-cd --version-file-strategy=recursive --corepack-enabled --resolve-engines)"
+
+               unalias l
       '';
 
       oh-my-zsh = {
@@ -131,16 +177,27 @@
       enable = true;
     };
 
-    kitty = {
+    alacritty = {
       enable = true;
-      themeFile = "Catppuccin-Latte";
-      # font = {
-      #   name = "FiraCode Nerd Font Mono";
-      #   package = pkgs.nerd-fonts.fira-code;
-      # };
 
       settings = {
-        enable_audio_bell = false;
+        font = {
+          normal = {
+            family = "FiraCode Nerd Font Mono";
+            style = "Regular";
+          };
+          bold = {
+            family = "FiraCode Nerd Font Mono";
+            style = "Bold";
+          };
+          italic = {
+            family = "FiraCode Nerd Font Mono";
+            style = "Italic";
+          };
+          size = 12.0;
+        };
+        env = {TERM = "xterm-256color";};
+        general = {live_config_reload = true;};
       };
     };
   };
