0f23365c6d47d3a204bb88ccdf4dcc9cf4463884
Peïo Thibault <peio.thibault@gmail.com>
2025-07-26 16:43:42 +0200

    ci: use cachix as nix cache (#15)

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -15,6 +15,16 @@
       trusted-users = ["root" "@wheel"];
       flake-registry = "";
       nix-path = config.nix.nixPath;
+
+      substituters = [
+        "https://cache.nixos.org"
+        "https://potb.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "potb.cachix.org-1:byvGn6qmFOaccjc7kbUMNKLJaCyn/B8HqGNG4gxI6P0="
+      ];
     };
 
     package = pkgs.nixVersions.latest;
