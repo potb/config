1a0003b8f56b46f9b92f8cb9522605c0656cb3f2
Peïo Thibault <peio.thibault@gmail.com>
2025-08-26 10:16:14 +0200

    feat: local dns to bypass restrictions

--- nixos/modules/networking.nix
+++ nixos/modules/networking.nix
@@ -1,7 +1,26 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   networking.hostName = "charon";
   networking.networkmanager.enable = true;
-  networking.nameservers = ["1.1.1.1"];
+  networking.networkmanager.dns = "none";
+  networking.nameservers = ["127.0.0.1"];
+
+  environment.systemPackages = [pkgs.cloudflared];
+
+  systemd.services.cloudflared-doh = {
+    description = "Cloudflared DoH";
+    after = ["network-online.target"];
+    wants = ["network-online.target"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.cloudflared}/bin/cloudflared proxy-dns --address 127.0.0.1 --port 53 --upstream https://1.1.1.1/dns-query --upstream https://1.0.0.1/dns-query";
+      Restart = "always";
+      RestartSec = 2;
+    };
+  };
 
   users.users.potb.extraGroups = lib.mkAfter ["networkmanager"];
 }
