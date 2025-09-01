971569297752099f3648ebec702533ad2bf614e8
Peïo Thibault <peio.thibault@gmail.com>
2025-09-01 14:18:19 +0200

    fix: properly share home-manager between darwin and nixos (#107)

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -1,5 +1,31 @@
 {lib, ...}: {
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-darwin";
 
+  networking.hostName = "nyx";
+
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  system.defaults = {
+    NSGlobalDomain = {
+      ApplePressAndHoldEnabled = false;
+      InitialKeyRepeat = 15;
+      KeyRepeat = 2;
+      "com.apple.keyboard.fnState" = true;
+    };
+    finder = {
+      AppleShowAllFiles = true;
+    };
+    dock = {
+      show-recents = false;
+      tilesize = 40;
+    };
+    CustomUserPreferences = {
+      "com.apple.HIToolbox" = {
+        AppleFnUsageType = 0;
+        AppleDictationAutoEnable = false;
+      };
+    };
+  };
+
   system.stateVersion = 5;
 }
