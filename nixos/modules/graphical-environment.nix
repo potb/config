6724d34aa2076c87a1954ad5905440623ec76f5c
Peïo Thibault <peio.thibault@gmail.com>
2025-10-04 14:54:24 +0200

    feat(xorg): remove sleep + faster keyboard repeat

--- nixos/modules/graphical-environment.nix
+++ nixos/modules/graphical-environment.nix
@@ -13,6 +13,11 @@
   services.xserver.enable = true;
   services.xserver.excludePackages = [pkgs.xterm];
   services.xserver.desktopManager.xterm.enable = false;
+
+  # Keyboard repeat settings: delay in ms before repeat starts, interval in ms between repeats
+  services.xserver.autoRepeatDelay = 200;
+  services.xserver.autoRepeatInterval = 80;
+
   services.xserver = {
     windowManager.i3 = {
       enable = true;
@@ -41,5 +46,14 @@
         symbolsFile = "${qwerty-fr}/share/X11/xkb/symbols/us_qwerty-fr";
       };
     };
+
+    # Disable screen blanking and DPMS
+    serverFlagsSection = ''
+      Option "BlankTime" "0"
+      Option "StandbyTime" "0"
+      Option "SuspendTime" "0"
+      Option "OffTime" "0"
+      Option "DPMS" "false"
+    '';
   };
 }
