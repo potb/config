e08a552ec5799f5e8fd3d172642bfeb1d1d20d85
Peïo Thibault <peio.thibault@gmail.com>
2026-01-29 01:49:10 +0100

    style: format nix code with alejandra

--- shared/modules/theme.nix
+++ shared/modules/theme.nix
@@ -2,11 +2,9 @@
   pkgs,
   lib,
   ...
-}:
-let
-  fonts = import ../fonts.nix { inherit pkgs; };
-in
-{
+}: let
+  fonts = import ../fonts.nix {inherit pkgs;};
+in {
   stylix =
     "${pkgs.base16-schemes}/share/themes/catppuccin-latte.yaml"
     |> (theme: {
