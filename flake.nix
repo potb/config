9ab6e5a1a352da4d689257c42a05ced676d08e4b
Peïo Thibault <peio.thibault@gmail.com>
2026-02-10 16:03:35 +0100

    feat(darwin): add nix-homebrew flake inputs and configuration

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "NixOS and nix-darwin configuration for charon and nyx";
 
   nixConfig = {
-    extra-substituters = ["https://potb.cachix.org"];
-    extra-trusted-public-keys = ["potb.cachix.org-1:byvGn6qmFOaccjc7kbUMNKLJaCyn/B8HqGNG4gxI6P0="];
+    extra-substituters = [ "https://potb.cachix.org" ];
+    extra-trusted-public-keys = [ "potb.cachix.org-1:byvGn6qmFOaccjc7kbUMNKLJaCyn/B8HqGNG4gxI6P0=" ];
   };
 
   inputs = {
@@ -42,142 +42,175 @@
       url = "github:cpick/nix-rosetta-builder";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nix-homebrew = {
+      url = "github:zhaofengli/nix-homebrew";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    nix-darwin,
-    nix-rosetta-builder,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    systems = [
-      "x86_64-linux"
-      "aarch64-darwin"
-    ];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      nix-darwin,
+      nix-rosetta-builder,
+      nix-homebrew,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
 
-    loadModulesFromDir = moduleDir:
-      builtins.readDir moduleDir
-      |> builtins.attrNames
-      |> builtins.filter (name: builtins.match ".+\\.nix$" name != null)
-      |> map (name: import (moduleDir + "/${name}"));
-
-    loadOverlays = overlaysDir:
-      if builtins.pathExists overlaysDir
-      then
-        builtins.readDir overlaysDir
+      loadModulesFromDir =
+        moduleDir:
+        builtins.readDir moduleDir
         |> builtins.attrNames
         |> builtins.filter (name: builtins.match ".+\\.nix$" name != null)
-        |> map (name: import (overlaysDir + "/${name}"))
-      else [];
+        |> map (name: import (moduleDir + "/${name}"));
 
-    getHomeManagerModules = platform: let
-      modulesDir = ./home-manager/modules;
-      platformDir = modulesDir + "/${platform}";
+      loadOverlays =
+        overlaysDir:
+        if builtins.pathExists overlaysDir then
+          builtins.readDir overlaysDir
+          |> builtins.attrNames
+          |> builtins.filter (name: builtins.match ".+\\.nix$" name != null)
+          |> map (name: import (overlaysDir + "/${name}"))
+        else
+          [ ];
 
-      loadNixPaths = dir:
-        builtins.readDir dir
-        |> builtins.attrNames
-        |> builtins.filter (name: builtins.match ".+\\.nix$" name != null)
-        |> map (name: dir + "/${name}");
+      getHomeManagerModules =
+        platform:
+        let
+          modulesDir = ./home-manager/modules;
+          platformDir = modulesDir + "/${platform}";
 
-      sharedModules = loadNixPaths modulesDir;
-      platformModules =
-        if builtins.pathExists platformDir
-        then loadNixPaths platformDir
-        else [];
+          loadNixPaths =
+            dir:
+            builtins.readDir dir
+            |> builtins.attrNames
+            |> builtins.filter (name: builtins.match ".+\\.nix$" name != null)
+            |> map (name: dir + "/${name}");
+
+          sharedModules = loadNixPaths modulesDir;
+          platformModules = if builtins.pathExists platformDir then loadNixPaths platformDir else [ ];
+        in
+        sharedModules ++ platformModules;
+
+      sharedOverlays = loadOverlays ./overlays;
+      darwinOverlays = loadOverlays ./darwin/overlays;
+      nixosOverlays = loadOverlays ./nixos/overlays;
+
+      darwinAllOverlays = sharedOverlays ++ darwinOverlays;
+      nixosAllOverlays = sharedOverlays ++ nixosOverlays;
     in
-      sharedModules ++ platformModules;
+    {
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
-    sharedOverlays = loadOverlays ./overlays;
-    darwinOverlays = loadOverlays ./darwin/overlays;
-    nixosOverlays = loadOverlays ./nixos/overlays;
+      checks = forAllSystems (system: {
+        deadnix =
+          nixpkgs.legacyPackages.${system}.runCommand "deadnix-check"
+            {
+              nativeBuildInputs = [ nixpkgs.legacyPackages.${system}.deadnix ];
+            }
+            ''
+              cd ${self}
+              deadnix --fail --no-lambda-pattern-names --no-lambda-arg
+              touch $out
+            '';
+      });
 
-    darwinAllOverlays = sharedOverlays ++ darwinOverlays;
-    nixosAllOverlays = sharedOverlays ++ nixosOverlays;
-  in {
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
-
-    checks = forAllSystems (system: {
-      deadnix =
-        nixpkgs.legacyPackages.${system}.runCommand "deadnix-check"
+      nixosConfigurations =
+        let
+          nixosModules = loadModulesFromDir ./nixos/modules;
+        in
         {
-          nativeBuildInputs = [nixpkgs.legacyPackages.${system}.deadnix];
-        }
-        ''
-          cd ${self}
-          deadnix --fail --no-lambda-pattern-names --no-lambda-arg
-          touch $out
-        '';
-    });
+          charon = nixpkgs.lib.nixosSystem {
+            specialArgs = { inherit inputs outputs; };
+            modules = nixosModules ++ [
+              ./nixos/configuration.nix
+              inputs.home-manager.nixosModules.home-manager
 
-    nixosConfigurations = let
-      nixosModules = loadModulesFromDir ./nixos/modules;
-    in {
-      charon = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules =
-          nixosModules
-          ++ [
-            ./nixos/configuration.nix
-            inputs.home-manager.nixosModules.home-manager
+              {
+                nixpkgs.overlays = nixosAllOverlays;
 
-            {
-              nixpkgs.overlays = nixosAllOverlays;
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.sharedModules = [ ];
+                home-manager.extraSpecialArgs = {
+                  inherit inputs;
+                };
 
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.sharedModules = [];
-              home-manager.extraSpecialArgs = {
-                inherit inputs;
-              };
+                home-manager.backupFileExtension = "backup";
 
-              home-manager.backupFileExtension = "backup";
+                home-manager.users.potb = {
+                  imports = getHomeManagerModules "linux";
+                  home.homeDirectory = nixpkgs.lib.mkForce "/home/potb";
+                };
+              }
+            ];
+          };
+        };
 
-              home-manager.users.potb = {
-                imports = getHomeManagerModules "linux";
-                home.homeDirectory = nixpkgs.lib.mkForce "/home/potb";
-              };
-            }
-          ];
-      };
+      darwinConfigurations =
+        let
+          darwinModules = loadModulesFromDir ./darwin/modules;
+        in
+        {
+          nyx = nix-darwin.lib.darwinSystem {
+            specialArgs = { inherit inputs outputs; };
+            modules = darwinModules ++ [
+              ./darwin/configuration.nix
+              inputs.home-manager.darwinModules.home-manager
+              nix-rosetta-builder.darwinModules.default
+              nix-homebrew.darwinModules.nix-homebrew
+
+              {
+                nixpkgs.overlays = darwinAllOverlays;
+
+                nix-rosetta-builder.enable = true;
+                nix-rosetta-builder.onDemand = true;
+
+                nix-homebrew = {
+                  enable = true;
+                  enableRosetta = true;
+                  user = "potb";
+                  taps = {
+                    "homebrew/homebrew-core" = inputs.homebrew-core;
+                    "homebrew/homebrew-cask" = inputs.homebrew-cask;
+                  };
+                  mutableTaps = false;
+                };
+
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.sharedModules = [ ];
+                home-manager.extraSpecialArgs = {
+                  inherit inputs;
+                };
+
+                home-manager.users.potb = {
+                  imports = getHomeManagerModules "darwin";
+                  home.homeDirectory = nixpkgs.lib.mkForce "/Users/potb";
+                };
+              }
+            ];
+          };
+        };
     };
-
-    darwinConfigurations = let
-      darwinModules = loadModulesFromDir ./darwin/modules;
-    in {
-      nyx = nix-darwin.lib.darwinSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules =
-          darwinModules
-          ++ [
-            ./darwin/configuration.nix
-            inputs.home-manager.darwinModules.home-manager
-            nix-rosetta-builder.darwinModules.default
-
-            {
-              nixpkgs.overlays = darwinAllOverlays;
-
-              nix-rosetta-builder.enable = true;
-              nix-rosetta-builder.onDemand = true;
-
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.sharedModules = [];
-              home-manager.extraSpecialArgs = {
-                inherit inputs;
-              };
-
-              home-manager.users.potb = {
-                imports = getHomeManagerModules "darwin";
-                home.homeDirectory = nixpkgs.lib.mkForce "/Users/potb";
-              };
-            }
-          ];
-      };
-    };
-  };
 }
