d5f04ffc39e8c4742617aeac2d6981f66867e66b
Peïo Thibault <peio.thibault@gmail.com>
2026-02-15 14:54:09 +0100

    fix(shell): remove platform-inappropriate zsh plugins

--- home-manager/modules/shell.nix
+++ home-manager/modules/shell.nix
@@ -9,34 +9,36 @@
     dotDir = "${config.xdg.configHome}/zsh";
 
     antidote.enable = true;
-    antidote.plugins = [
-      "mattmc3/ez-compinit"
-      "zsh-users/zsh-completions kind:fpath path:src"
-      "getantidote/use-omz"
-      "ohmyzsh/ohmyzsh path:lib"
-      "ohmyzsh/ohmyzsh path:plugins/archlinux"
-      "ohmyzsh/ohmyzsh path:plugins/aws"
-      "ohmyzsh/ohmyzsh path:plugins/colored-man-pages"
-      "ohmyzsh/ohmyzsh path:plugins/common-aliases"
-      "ohmyzsh/ohmyzsh path:plugins/docker"
-      "ohmyzsh/ohmyzsh path:plugins/docker-compose"
-      "ohmyzsh/ohmyzsh path:plugins/extract"
-      "ohmyzsh/ohmyzsh path:plugins/eza"
-      "ohmyzsh/ohmyzsh path:plugins/fancy-ctrl-z"
-      "ohmyzsh/ohmyzsh path:plugins/git"
-      "ohmyzsh/ohmyzsh path:plugins/gpg-agent"
-      "ohmyzsh/ohmyzsh path:plugins/magic-enter"
-      "ohmyzsh/ohmyzsh path:plugins/node"
-      "ohmyzsh/ohmyzsh path:plugins/ssh"
-      "ohmyzsh/ohmyzsh path:plugins/ssh-agent"
-      "ohmyzsh/ohmyzsh path:plugins/sudo"
-      "ohmyzsh/ohmyzsh path:plugins/starship"
-      "ohmyzsh/ohmyzsh path:plugins/systemd"
-      "ohmyzsh/ohmyzsh path:plugins/transfer"
-      "ohmyzsh/ohmyzsh path:plugins/zoxide"
-      "zsh-users/zsh-autosuggestions"
-      "zdharma-continuum/fast-syntax-highlighting"
-    ];
+    antidote.plugins =
+      [
+        "mattmc3/ez-compinit"
+        "zsh-users/zsh-completions kind:fpath path:src"
+        "getantidote/use-omz"
+        "ohmyzsh/ohmyzsh path:lib"
+        "ohmyzsh/ohmyzsh path:plugins/aws"
+        "ohmyzsh/ohmyzsh path:plugins/colored-man-pages"
+        "ohmyzsh/ohmyzsh path:plugins/common-aliases"
+        "ohmyzsh/ohmyzsh path:plugins/docker"
+        "ohmyzsh/ohmyzsh path:plugins/docker-compose"
+        "ohmyzsh/ohmyzsh path:plugins/extract"
+        "ohmyzsh/ohmyzsh path:plugins/eza"
+        "ohmyzsh/ohmyzsh path:plugins/fancy-ctrl-z"
+        "ohmyzsh/ohmyzsh path:plugins/git"
+        "ohmyzsh/ohmyzsh path:plugins/gpg-agent"
+        "ohmyzsh/ohmyzsh path:plugins/magic-enter"
+        "ohmyzsh/ohmyzsh path:plugins/node"
+        "ohmyzsh/ohmyzsh path:plugins/ssh"
+        "ohmyzsh/ohmyzsh path:plugins/ssh-agent"
+        "ohmyzsh/ohmyzsh path:plugins/sudo"
+        "ohmyzsh/ohmyzsh path:plugins/starship"
+        "ohmyzsh/ohmyzsh path:plugins/transfer"
+        "ohmyzsh/ohmyzsh path:plugins/zoxide"
+        "zsh-users/zsh-autosuggestions"
+        "zdharma-continuum/fast-syntax-highlighting"
+      ]
+      ++ lib.optionals pkgs.stdenv.isLinux [
+        "ohmyzsh/ohmyzsh path:plugins/systemd"
+      ];
 
     initContent = lib.mkBefore ''
       zstyle ':omz:plugins:eza' 'dirs-first' yes
