fbf03ad47d8eb2d7066275b9c87e834b33f152f9
Peïo Thibault <peio.thibault@gmail.com>
2026-02-19 11:28:46 +0100

    feat: add 1password gui on both darwin and nixos

--- nixos/modules/core-services.nix
+++ nixos/modules/core-services.nix
@@ -1,3 +1,9 @@
 {...}: {
   services.gnome.gnome-keyring.enable = true;
+
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = ["potb"];
+  };
 }
