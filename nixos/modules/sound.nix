926883d3729d82feeb6a32398d0c891b9274b146
Peïo Thibault <peio.thibault@gmail.com>
2026-03-05 19:18:31 +0100

    feat(sound): add easyeffects with low-latency pipewire config

--- nixos/modules/sound.nix
+++ nixos/modules/sound.nix
@@ -14,6 +14,21 @@
       "01-buffer-underrun" = {
         link.max-buffers = 64;
       };
+
+      "02-low-latency" = {
+        "context.properties" = {
+          "default.clock.rate" = 48000;
+          "default.clock.quantum" = 64;
+          "default.clock.min-quantum" = 64;
+          "default.clock.max-quantum" = 128;
+        };
+      };
+
+      "03-mono-upmix" = {
+        "context.properties" = {
+          "channelmix.upmix" = true;
+        };
+      };
     };
   };
 }
---
+++ overlays/easyeffects.nix
@@ -0,0 +1,9 @@
+final: prev: {
+  easyeffects = prev.easyeffects.overrideAttrs (old: {
+    patches =
+      (old.patches or [])
+      ++ [
+        ./easyeffects-save-crash.patch
+      ];
+  });
+}
