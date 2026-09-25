cc4c01bdd905eacc651824659d30182f0f3d8693
Peïo Thibault <peio.thibault@gmail.com>
2026-09-25 22:31:49 +0200

    charon: authorize new-horizons SSH key

--- hosts/charon/configuration.nix
+++ hosts/charon/configuration.nix
@@ -1,5 +1,9 @@
-{lib, ...}: {
+{lib, ...}: let
+  keys = import ../../shared/keys.nix;
+in {
   system.stateVersion = "24.11";
+
+  users.users.potb.openssh.authorizedKeys.keys = [keys.new-horizons];
 
   nix.settings = {
     max-jobs = 4;
--- shared/keys.nix
+++ shared/keys.nix
@@ -2,4 +2,5 @@
   charon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPEvgHXpApOdOWFe5bKuZW4M3adoAvcDqFCaP7bYPkDu potb@charon";
   nyx = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBNj+8QbPM+G7odRtOWOWZ/A+UQ6FvnYMnurBgXWXfk potb@nyx";
   kerberos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAmy7vOw/pHg/H41HtEjlapdHm+nWMRhhMmqqiIuFIP+ potb@kerberos";
+  new-horizons = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGxlxiQwn4tzGwT2mGfgNbZcTh+Et8cuUdZV78XdKZvI potb@new-horizons";
 }
